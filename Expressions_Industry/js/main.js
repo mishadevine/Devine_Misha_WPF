744042eb7b1fcbfbd199cc9a7279b83e47f86555
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-10 23:43:40 -0400

    Expressions_Industry
    var created as prompt to ask user what the weight of the image is.

--- Expressions_Industry/js/main.js
+++ Expressions_Industry/js/main.js
@@ -1,1 +1,4 @@
 //Misha Devine September 10th 2014 Web Programming Fundamentals
+
+//Calculate how many square pixels are in an image using its weight, height and DPI
+var weight = prompt("What is the weight of the image?");
