d81271ddeaf2dbf07129c6e95c67776a2b73ffbf
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-10 21:36:26 -0400

    Expressions_Personal
    Bills totaled up.

--- Expressions_Personal/js/main.js
+++ Expressions_Personal/js/main.js
@@ -1,1 +1,16 @@
 //Misha Devine September 10th 2014 Web Programming Fundamentals
+
+//Calculate how much money is needed from September to December
+
+var rent = 485;
+var carInsurance = 54;
+var cellPhone = 114;
+var gas = 140
+var monthsNeeded = 4;
+
+//Total bills
+var totalBills = rent + carInsurance + cellPhone + gas;
+//output for totalBills
+console.log("The total ammount of all bills added up is $"+totalBills);
+
+
