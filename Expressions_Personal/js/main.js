4486c3eae78360e29c8d7285af14100cee7ef763
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-10 21:41:19 -0400

    Expressions_personal
    The total amount of money needed from september to december found.

--- Expressions_Personal/js/main.js
+++ Expressions_Personal/js/main.js
@@ -1,6 +1,6 @@
 //Misha Devine September 10th 2014 Web Programming Fundamentals
 
-//Calculate how much money is needed from September to December
+//Calculate how much money is needed from September to December when you have $1000 to start.
 
 var rent = 485;
 var carInsurance = 54;
@@ -11,6 +11,11 @@ var monthsNeeded = 4;
 //Total bills
 var totalBills = rent + carInsurance + cellPhone + gas;
 //output for totalBills
-console.log("The total ammount of all bills added up is $"+totalBills);
+console.log("The total amount of all bills added up is $"+totalBills);
+
+//Multiply totalBills * monthsNeeded
+var grandTotal = totalBills * monthsNeeded;
+//output for grandTotal
+console.log("The grand total amount of money needed for September to December is $"+grandTotal);
 
 
