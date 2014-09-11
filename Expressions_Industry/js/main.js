a0198643121a8f8e1205ce3d45e302f54095292a
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-10 23:45:27 -0400

    Comment made
    Comment made to show what the next line will be

--- Expressions_Industry/js/main.js
+++ Expressions_Industry/js/main.js
@@ -1,4 +1,9 @@
 //Misha Devine September 10th 2014 Web Programming Fundamentals
 
 //Calculate how many square pixels are in an image using its weight, height and DPI
+
+//Prompt made to ask user weight of image
 var weight = prompt("What is the weight of the image?");
+
+//Prompt made to ask user height of image
+
