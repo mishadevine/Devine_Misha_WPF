8494846cd3c7b8fa32420191231fb0abb7942dcb
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-10 22:39:16 -0400

    Expressions_Wacky
    Givens assigned

--- Expressions_Wacky/js/main.js
+++ Expressions_Wacky/js/main.js
@@ -1,1 +1,7 @@
 //Misha Devine September 10th 2014 Web Programming Fundamentals
+
+//Calculate how much money you made using your hourly rate, amount of months and amount of hours worked every week.
+
+var hourlyRate = prompt("What is your hourly rate?");
+var hoursWorked = prompt("How many hours do you work weekly?")
+var monthsWorked = prompt("How many months have you worked?");
