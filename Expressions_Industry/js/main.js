b9d2ae364e07ed29a6eb90416dcb1c617a472a2d
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-10 23:59:22 -0400

    Width in pixel dimensions
    The width in pixel dimensions has been identified

--- Expressions_Industry/js/main.js
+++ Expressions_Industry/js/main.js
@@ -1,15 +1,20 @@
 //Misha Devine September 10th 2014 Web Programming Fundamentals
 
-//Calculate how many square pixels are in an image using its weight, height and DPI
+//Calculate how many total square pixels are in an image using its weight, height and DPI
 
 //Prompt made to ask user weight of image
-var weight = prompt("What is the weight of the image?");
+var width = prompt("What is the width in inches of the image?");
 
 //Prompt made to ask user height of image
-var height = prompt("What is the weight of the image?");
+var height = prompt("What is the height in inches of the image?");
 
 //Prompt made to ask user the DPI of image
 var dpi = prompt("What is the DPI of the image?");
 
+
+//Multiple the width * height
+var widthInPixels = width * dpi;
+//output of withInPixels
+console.log("The width of the image in pixel dimensions is"+" "+widthInPixels);
 
 
