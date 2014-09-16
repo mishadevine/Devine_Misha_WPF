265bfeb2fb6257a9d30f71029b35ca5aa97fbd1d
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-15 21:02:16 -0400

    personal
    if, console log, and prompt created

--- Day6 - Conditional Assignment/Conditionals_Personal/js/animals.js
+++ Day6 - Conditional Assignment/Conditionals_Personal/js/animals.js
@@ -5,4 +5,10 @@ var dogs = "dogs";
 
 var pets1 = Number(prompt("How many"+" "+dogs+" "+"do you have?"));
 
+if(pets1==""){
+    console.log("You did not enter a number.");
+    pets1 = Number(prompt("Please enter a number."));
+}
+
+
 
