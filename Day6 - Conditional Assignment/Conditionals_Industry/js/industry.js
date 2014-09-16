68b777c15ba59e6d2d11cd46e32f7ac3eac2095a
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-16 00:10:09 -0400

    industry - completed
    tenery changed

--- Day6 - Conditional Assignment/Conditionals_Industry/js/industry.js
+++ Day6 - Conditional Assignment/Conditionals_Industry/js/industry.js
@@ -6,6 +6,8 @@ var state = "Florida";
 var floridaHigh = Number(92200);
 var usHigh = Number(110400);
 
+//prompt to ask user if they are a web developer
+
 //ask user what is the state they live in
 var livesIn = prompt("What state do you live in?");
 //check to make sure a number has been entered
@@ -15,6 +17,7 @@ if(livesIn===""){
     livesIn = Number(prompt("Please enter the state you live in"));
 }
 //if state is florida they will make $92,200 a year
-(livesIn===state)?console.log("You can make a potential high of $"+floridaHigh+" "+"being a Web Developer."):console.log("The potential high in the US is $"+usHigh+". Use http://www.onetonline.org/link/summary/15-1134.00#WagesEmployment to find out the high in your state.");
+(livesIn===state)?console.log("You can make a potential high of $"+floridaHigh+" "+"being a Web Developer."):console.log("The potential high in the US is $"+usHigh+". Use other resources to find out the high in your state.");
+
 
 
