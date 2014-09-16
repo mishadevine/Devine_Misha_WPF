885d39fd5e0638e28f3f02f9179786ff2960d0d0
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-15 23:48:05 -0400

    var prompt and if statement
    a var prompt has been added to ask the user what state they live in.
    another prompt has been added to re ask the user what state do they
    live in.

--- Day6 - Conditional Assignment/Conditionals_Industry/js/industry.js
+++ Day6 - Conditional Assignment/Conditionals_Industry/js/industry.js
@@ -6,4 +6,13 @@ var state = "Florida";
 var floridaHigh = Number(92200);
 var usHigh = Number(110400);
 
+//ask user what is the state they live in
+var livesIn = Number(prompt("What state do you live in?"));
+//check to make sure a number has been entered
+if(livesIn===""){
+    console.log("You did not enter your state.");
+    //prompt to reask them to enter their state and to not leave it blank
+    livesIn = Number(prompt("Please enter the state you live in"));
+}
+
 
