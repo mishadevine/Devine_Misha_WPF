5ba6028c1b60f38b6dc17c88a1548c70c540e435
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-15 23:40:31 -0400

    Conditionals_Industry vars
    givens were made as vars

--- Day6 - Conditional Assignment/Conditionals_Industry/js/industry.js
+++ Day6 - Conditional Assignment/Conditionals_Industry/js/industry.js
@@ -1,1 +1,9 @@
 //Misha Devine Web Programming Fundamentals
+
+//Calculate how much money a web developer would make in Florida versus the united states as a whole
+//these are the givens
+var state = "Florida";
+var floridaHigh = Number(92200);
+var usHigh = Number(110400);
+
+
