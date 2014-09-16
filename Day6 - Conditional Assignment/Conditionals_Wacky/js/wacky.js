94e680f22a65db927334849eefcaceaa242f3965
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-15 22:14:02 -0400

    wacky vars and ifs
    vars prompts and ifs statements have been created to ask the user a
    question and to test a value has been entered

--- Day6 - Conditional Assignment/Conditionals_Wacky/js/wacky.js
+++ Day6 - Conditional Assignment/Conditionals_Wacky/js/wacky.js
@@ -1,3 +1,20 @@
-/**
- * Created by mdevine on 9/15/14.
- */
+//Misha Devine Web Programming Fundamentals
+
+//Calculate if someone is old enough to drink
+var drinkingAge = 21;
+var state = "Florida";
+
+//ask user what is their age
+var userAge = Number(prompt("What is your age?"));
+//test to see if a number has been entered
+if(userAge==""){
+    console.log("You did not enter your age.");
+    userAge = Number(prompt("Please enter your age."));
+}
+//ask user what state do they live in
+var yourState = prompt("What state do you live in?");
+//test to see if a string has been entered
+if(yourState===""){
+    console.log("You did not enter your state.");
+    yourState = prompt("Please enter the state you live in.")
+}
