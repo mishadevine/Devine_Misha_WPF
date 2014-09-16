0163d8c094a075649fb5a43edf86ad1bf17336bb
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-15 21:10:45 -0400

    personal prompts
    a prompt was created asking the user how many cats they have

--- Day6 - Conditional Assignment/Conditionals_Personal/js/animals.js
+++ Day6 - Conditional Assignment/Conditionals_Personal/js/animals.js
@@ -1,17 +1,23 @@
 // Misha Devine Web Programming Fundamentals
 
+//givens
 var cats = "cats";
 var dogs = "dogs";
 
+//ask person how many dogs they have
 var pets1 = Number(prompt("How many"+" "+dogs+" "+"do you have?"));
-
+//test if a number has been entered
 if(pets1==""){
     console.log("You did not enter a number.");
     pets1 = Number(prompt("Please enter a number."));
 }
+//isNaN
 if(isNaN(pets1)||pets1==""){
     pets1 = Number(prompt("Please enter a number and please do not leave blank."));
 }
+
+var pets2 =Number(prompt("How many"+" "+cats+" "+"do you have?"));
+
 
 
 
