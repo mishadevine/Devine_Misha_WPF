58f6311442c62298f271fd63f3abbcab34a887ad
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-17 23:43:22 -0400

    DoWhileLoop and WhileLoop completed
    the worksheets for DoWhileLoop and WhileLoop are completed

--- Loops_Worksheet/js/DoWhileLoop.js
+++ Loops_Worksheet/js/DoWhileLoop.js
@@ -1,1 +1,20 @@
 //Misha Devine September 17, 2014 Loops_Worksheet
+
+alert("The computer is finding a random number between 0 and 20 and telling you how many times it took to find the 20");
+
+var runProgram = true;
+
+var i = 0;
+//set a variable to hold our value
+
+do{
+    var random =Math.ceil(Math.random()*(Number(20)-Number(0)));
+    console.log(random);
+    i++;
+    if(random === 20){
+        runProgram = false;
+        console.log("You rolled a 20! It took "+i+" times!");
+    }
+}
+while(runProgram);
+
--- Loops_Worksheet/js/WhileLoop.js
+++ Loops_Worksheet/js/WhileLoop.js
@@ -1,1 +1,23 @@
 //Misha Devine September 17, 2014 Loops_Worksheet
+
+//calculate if the user is old enough to be in Islands of Adventure by them self
+
+var minAge = parseInt(14);
+
+//ask the user how old are they
+var age = parseInt(prompt("How old are you?"));
+
+//the while loop will tell keep telling the user "Please enter your age and do not leave blank" until a number is entered
+while(isNaN(age) || age==="") {
+    age = parseInt(prompt("Please enter your age and do not leave blank."));
+}
+
+if(age >= minAge){
+    //if age is greater than or equal to min age the user can enter IOA by themselves
+    console.log("You may enter Islands of Adventure by yourself!")
+}
+else {
+    //if age is less than min age the user cannot enter IOA by themselves
+    //but can be accompanied by a parent
+    console.log("You need a parent to accompany you into Islands of Adventure.");
+}
