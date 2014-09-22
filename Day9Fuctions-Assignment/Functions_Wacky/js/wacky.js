47d9d8931b14b255c3d8ccabf58c3fbb28817947
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-22 19:21:38 -0400

    Functions_Wacky problem created
    A problem has been created to solve

--- Day9Fuctions-Assignment/Functions_Wacky/js/wacky.js
+++ Day9Fuctions-Assignment/Functions_Wacky/js/wacky.js
@@ -1,3 +1,5 @@
-/**
- * Created by mdevine on 9/22/14.
- */
+//Misha Devine Web Programming Fundamentals September 22, 2014
+
+//You are collecting quarters from every state.
+//You have a map that can hold up to 50 quarters; 1 for each state.
+//Calculate how much money you will have if you collect all 50 states.
