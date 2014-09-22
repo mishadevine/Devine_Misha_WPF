70ce82b07b6333b3d866c4725939291280e21285
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-22 19:31:43 -0400

    Functions_Wacky vars
    vars have been made to use within the problem

--- Day9Fuctions-Assignment/Functions_Wacky/js/wacky.js
+++ Day9Fuctions-Assignment/Functions_Wacky/js/wacky.js
@@ -3,3 +3,6 @@
 //You are collecting quarters from every state.
 //You have a map that can hold up to 50 quarters; 1 for each state.
 //Calculate how much money you will have if you collect all 50 states.
+
+var states = 50;
+var quarterPrice = .25;
