696cb47df797e492b5b315665fbe3a87f6eb0746
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-22 22:48:15 -0400

    Functions_Industry vars created
    cars have been created to ask the user how many signs and flyers they
    have completed

--- Day9Fuctions-Assignment/Functions_Industry/js/industry.js
+++ Day9Fuctions-Assignment/Functions_Industry/js/industry.js
@@ -1,3 +1,6 @@
-/**
- * Created by mdevine on 9/22/14.
- */
+//Misha Devine Web Programming Fundamentals September 22, 2014
+
+
+var flyers = parseInt(prompt("How many flyers have you completed in 24 hours?"));
+var signs = parseInt(prompt("How many signs have you completed in 24 hours?"));
+
