ee0b691142beaa243a3cb123218e2094a4c56617
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-22 22:00:35 -0400

    if statement has been created
    and if statement has been created for if the person is 17 or older and
    wants to see a r rated movie

--- Day9Fuctions-Assignment/Functions_Personal/js/personal.js
+++ Day9Fuctions-Assignment/Functions_Personal/js/personal.js
@@ -6,4 +6,15 @@ var movie = prompt("Is your movie R Rated or PG 13? ");
 var rRated = "R Rated";
 var pg13 = "PG 13";
 
+//anonymous function begings
+var oldEnough = function (age,movie,rRated,pg13) {
+
+    //if users age is 17 or older and they want to see a
+    // rated r movie it outputs they are old enough to see it
+    if(age >=17 && movie===rRated){
+        console.log("You are old enough to see this movie without an adult.");
+    }
+    
+
+}
 
