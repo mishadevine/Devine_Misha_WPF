1f28ff64618b01cad5f82e6b21844b7f9909ea7d
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-22 21:50:34 -0400

    Functions_Personal vars created
    vars created for the problem

--- Day9Fuctions-Assignment/Functions_Personal/js/personal.js
+++ Day9Fuctions-Assignment/Functions_Personal/js/personal.js
@@ -1,2 +1,9 @@
 //Misha Devine Web Programming Fundamentals September 22, 2014
 
+//are you old enough to see this movie
+var age = parseInt(prompt("How old are you?"));
+var movie = prompt("Is your movie R Rated or PG 13? ");
+var rRated = "R Rated";
+var pg13 = "PG 13";
+
+
