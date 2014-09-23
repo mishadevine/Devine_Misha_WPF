6505747be6b2bb38b6b11f88ffa99977a074e45a
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2014-09-22 22:50:51 -0400

    function has been created
    a function has been created to calculate the flyers and signs

--- Day9Fuctions-Assignment/Functions_Industry/js/industry.js
+++ Day9Fuctions-Assignment/Functions_Industry/js/industry.js
@@ -6,4 +6,11 @@ var signs = parseInt(prompt("How many signs have you completed in 24 hours?"));
 //Call the function
 var total = totalProducts(flyers, signs);
 
+//the fucntion starts
+function totalProducts(f, s){
+    //adding flyers and signs together
+    var totalProducts = f+s;
+    //returning the calculation outside the function
+    return totalProducts;
 
+}
